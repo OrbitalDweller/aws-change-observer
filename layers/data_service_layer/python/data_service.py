894d2e1e386dca9c07cfa7ca1f82b0e585d24d47
PCBush <pcninjarm@gmail.com>
2024-11-07 14:32:57 -0600

    implemented get_markers, add_marker, delete_marker

--- layers/data_service_layer/python/data_service.py
+++ layers/data_service_layer/python/data_service.py
@@ -24,4 +24,55 @@ class DataService:
         :return: A list of markers.
         :raises NotImplementedError: Indicates the method is not yet implemented.
         """
-        raise NotImplementedError("This method is not implemented yet.")
+        try: 
+            response = self.table.scan()
+            markers = response.get('Items', [])
+            return markers
+        except Exception as e:
+            logger.error(f"Error retrieving markers")
+
+    def add_marker(self, markerId, name, longitude, latitude, imgurl, tags):
+        """
+        Add a new marker to the DynamoDB table.
+
+        :param markerId: Unique identifier for the location.
+        :param Name: Name of the location.
+        :param Longitude: Longitude coordinate of the location.
+        :param Latitude: Latitude coordinate of the location.
+        :param ImgUrl: URL to an image of the location.
+        :param Tags: Tags associated with the location.
+        :return: The response from DynamoDB.
+        """
+        response = self.table.put_item(
+            Item={
+                'markerId': str(markerId),  #dynamoDB schema requires string here
+                'name': name,
+                'longitude': longitude,
+                'Latitude': latitude,  
+                'ImgUrl': imgurl,
+                'Tags': tags
+            }
+        )
+        logger.info(f"Marker added: {name} (ID: {markerId})")
+        return response  #response for debugging
+
+
+    def delete_marker(self, markerId):
+        """
+        Delete a marker from the DynamoDB table.
+
+        :param markerId: Unique identifier for the location to delete.
+        :return: The response from DynamoDB.
+        """
+        try:
+            response = self.table.delete_item(
+                Key={
+                    'markerId': str(markerId)  #dynamoDB schema requires string here
+                }
+            )
+            logger.info(f"Marker with markerId {markerId} deleted.")
+            return response
+        except Exception as e:
+            logger.error(f"Error deleting marker with markerId {markerId}: {e}")
+            return None
+        
